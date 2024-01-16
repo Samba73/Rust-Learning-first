4e7689c7534ced9f767116812096662c7304c853
Samba <sambasivam.k@gmail.com>
2024-01-16 11:06:27 +0530

    closure-step 1

---
+++ src/helpers.rs
@@ -0,0 +1,15 @@
+use std::io;
+pub fn driving_age() -> bool {
+    let age_to_drive = 16u8;
+
+    println!("Enter your age to determine the driving access");
+
+    let mut user_age = String::new();
+    io::stdin().read_line(&mut user_age).expect("Unable to read input");
+    let age = user_age.trim().parse::<u8>().unwrap();
+    if age >= age_to_drive {
+        true
+    } else {
+        false
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 use mystructs::{Rectangle,Person};
-
+//use helpers::driving_age;
 use crate::mystructs::myenums::State;
 
-
-mod mystructs;
+pub mod helpers;
+pub mod mystructs;
 fn main() {
     let rect = Rectangle::new(10,20);
 
@@ -13,4 +13,21 @@ fn main() {
     "9999999".to_string(),"samba@gmail.com".to_string(),State::TN);
 
     p.print_person();
+
+    //println!("Driving Eligibility is {}",driving_age());
+
+    let clsr2 = |x: i32, y| x + y;
+    let clsr = clsr2;
+    println!("The sum of 2 numbers are {}", clsr(2,5));
+    //println!("Concatenate strings {}",add2("Hello","World"));
+
+    let x = 10;
+    let y = 5;
+    let clsr1 = || x * y;
+  
+    let clsr = clsr1;
+    //let y = 5;
+
+    println!("The product of 2 numbers {} and {} is {}", x, y, clsr());
+
 }
